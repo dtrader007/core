599470f161e5f18c9ff95d8edef07408ebe92412
Jake Fowler <fowlerjake@hotmail.com>
2019-08-23 09:27:36 +0100

    Added calculation of adjustment term to trinomial tree calc.

--- src/Cmdty.Core.Trees/OneFactorTrinomialTree.cs
+++ src/Cmdty.Core.Trees/OneFactorTrinomialTree.cs
@@ -30,16 +30,22 @@ using Cmdty.TimeSeries;
 
 namespace Cmdty.Core.Trees
 {
+    /// <summary>
+    /// Class with method used to construct one-factor lognormal trinomial tree fitted to an initial forward curve.
+    /// References:
+    /// Hull J, and A White, 1994, “Numerical Procedures For Implementing Term Structure Models I: Single-Factor Models, The Journal of Derivatives, Fall, pp. 7-16.
+    /// Clewlow L and Strickland C, "Valuing Energy Options in a One Factor Model Fitted to Forward Prices", April 1999, Available at SSRN: https://ssrn.com/abstract=160608 or http://dx.doi.org/10.2139/ssrn.160608
+    /// </summary>
     public static class OneFactorTrinomialTree
     {
 
-        public static TimeSeries<T, IReadOnlyList<TreeNode>> CreateTree<T>(TimeSeries<T, double> forwardCurve, double lambda, 
+        public static TimeSeries<T, IReadOnlyList<TreeNode>> CreateTree<T>(TimeSeries<T, double> forwardCurve, double meanReversion, 
                                                                     TimeSeries<T, double> volatility, double onePeriodTimeDelta)
             where T : ITimePeriod<T>
         {
             // TODO check forwardCurve for null
-            if (lambda <= 0) // TODO allow to be zero for non-mean reverting case?
-                throw new ArgumentException("Mean reversion must be positive.", nameof(lambda));
+            if (meanReversion <= 0) // TODO allow to be zero for non-mean reverting case?
+                throw new ArgumentException("Mean reversion must be positive.", nameof(meanReversion));
 
             if (onePeriodTimeDelta <= 0)
                 throw new ArgumentException("Time delta must be positive.", nameof(onePeriodTimeDelta));
@@ -56,23 +62,28 @@ namespace Cmdty.Core.Trees
 
             int numPeriods = forwardCurve.Count;
 
-            int maxNumTreeLevels = 0; // TODO calc = jmax * 2 + 1
+            double expectedOuReturn = Math.Exp(-meanReversion * onePeriodTimeDelta) - 1;    // Equal to M in Hull & White 1994, as calculated in end note 5, p16
+            
+            // For jMax calc see Hull & White p12
+            int jMax = Convert.ToInt32(Math.Ceiling(-0.184 / expectedOuReturn));
+
+            int maxNumTreeLevels = jMax * 2 + 1;
 
             var transitionProbabilities = new double[numPeriods - 1][][];
 
 
-            // Calculate node prices using forward induction
+            // Calculate the probability of reaching each node using forward induction
             var nodeProbabilities = new double[numPeriods][];
-            var nodePrices = new double[numPeriods][];
+            var nodeOuProcessValues = new double[numPeriods][];
 
             nodeProbabilities[0] = new[] {1.0}; // Current point as probability 1
 
             for (int i = 0; i < numPeriods - 1; i++) // Loop forward through time
             {
-                int currentStepNumLevels = nodePrices[i].Length;
-                int nextStepNumLevels = nodePrices[i + 1].Length;
+                int currentStepNumLevels = nodeOuProcessValues[i].Length;
+                int nextStepNumLevels = nodeOuProcessValues[i + 1].Length;
                 nodeProbabilities[i + 1] = new double[nextStepNumLevels];
-                bool treeHasReachedWidestPoint = nodePrices[i].Length == maxNumTreeLevels;
+                bool treeHasReachedWidestPoint = nodeOuProcessValues[i].Length == maxNumTreeLevels;
 
                 for (int j = 0; j < currentStepNumLevels; j++) // Loop through the tree price levels
                 {
@@ -88,37 +99,52 @@ namespace Cmdty.Core.Trees
 
             }
 
+            // Calculate adjustment factor to ensure expected spot price equals current forward price
+            // See Clewlow and Strickland equation 4.10 with discount factor cancelled out
+            var adjustmentTerms = new double[numPeriods];
+            for (int i = 0; i < numPeriods; i++)
+            {
+                double expectedExponentialOfOu = 0;
+                for (int j = 0; j < nodeProbabilities[i].Length; j++)
+                {
+                    expectedExponentialOfOu += nodeProbabilities[i][j] * Math.Exp(nodeOuProcessValues[i][j]); // TODO adjust for volatility?
+                }
+                double forwardPrice = forwardCurve[i];
+                adjustmentTerms[i] = Math.Log(forwardPrice / expectedExponentialOfOu);
+            }
 
             // Populate results
             var resultNodes = new TreeNode[numPeriods][];
 
             // Populate nodes at end, with no forward transitions
             int lastPeriodIndex = numPeriods - 1;
-            int numLevelsAtEnd = nodePrices[lastPeriodIndex].Length;
+            int numLevelsAtEnd = nodeOuProcessValues[lastPeriodIndex].Length;
             resultNodes[lastPeriodIndex] = new TreeNode[numLevelsAtEnd];
             for (int j = 0; j < numLevelsAtEnd; j++) // Loop through price levels
             {
-                resultNodes[lastPeriodIndex][j] = new TreeNode(nodePrices[lastPeriodIndex][j], 
+                double nodeSpotPrice = Math.Exp(nodeOuProcessValues[lastPeriodIndex][j] + adjustmentTerms[lastPeriodIndex]);
+
+                resultNodes[lastPeriodIndex][j] = new TreeNode(nodeSpotPrice,
                         nodeProbabilities[lastPeriodIndex][j], new NodeTransition[0]);
             }
 
             // Populate nodes at all other time steps
             for (int i = numPeriods - 2; i >= 0; i--) // Loop back through time periods
             {
-                bool treeHasReachedWidestPoint = nodePrices[i].Length == maxNumTreeLevels;
-                for (int j = 0; j < nodePrices[i].Length; j++) // Loop through price levels
+                bool treeHasReachedWidestPoint = nodeOuProcessValues[i].Length == maxNumTreeLevels;
+                for (int j = 0; j < nodeOuProcessValues[i].Length; j++) // Loop through price levels
                 {
-                    (int nextPeriodTopNodeIndex,
-                            int nextPeriodMiddleNodeIndex,
-                            int nextPeriodBottomNodeIndex) =
-                        GetNextStepIndexPositions(j, treeHasReachedWidestPoint, maxNumTreeLevels);
+                    (int nextPeriodTopNodeIndex, int nextPeriodMiddleNodeIndex,int nextPeriodBottomNodeIndex) =
+                                            GetNextStepIndexPositions(j, treeHasReachedWidestPoint, maxNumTreeLevels);
 
                     var topTransition = new NodeTransition(transitionProbabilities[i][j][2], resultNodes[i + 1][nextPeriodTopNodeIndex]);
                     var middleTransition = new NodeTransition(transitionProbabilities[i][j][1], resultNodes[i + 1][nextPeriodMiddleNodeIndex]);
                     var bottomTransition = new NodeTransition(transitionProbabilities[i][j][0], resultNodes[i + 1][nextPeriodBottomNodeIndex]);
                     var nodeTransitions = new NodeTransition[] {bottomTransition, middleTransition, topTransition};
 
-                    resultNodes[i][j] = new TreeNode(nodePrices[i][j], nodeProbabilities[i][j], nodeTransitions);
+                    double nodeSpotPrice = Math.Exp(nodeOuProcessValues[i][j] + adjustmentTerms[i]);
+
+                    resultNodes[i][j] = new TreeNode(nodeSpotPrice, nodeProbabilities[i][j], nodeTransitions);
                 }
 
             }
